62481b25956ff2f8a31a5fbb8000760a8e979e6b
00784094462 <ronaldo.veras@serpro.gov.br>
2019-09-05 08:19:12 -0300

    material de estudo

---
+++ visits/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+client.set('visits', 0);
+app.get('/', (req, resp) => {
+    client.get('visits', (err, visits) => {
+        resp.send(visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen('8081', () => {
+    console.log('8081');
+});
